f153653f34df77e9d84a3416d7310dd0427a35ad
FelipeThim <162934100+FelipeThim@users.noreply.github.com>
2026-06-07 16:47:24 -0300

    Update robo.ino

--- robo.ino
+++ robo.ino
@@ -1,40 +1,42 @@
 #include <Wire.h>
+#include <Servo.h>
 #include <HTInfraredSeeker.h>
 
-// Ph_1 == Left Ponte H
-// Ph_2 == Right Ponte H
+// Ponte H 1
+#define in_1_ph_1 25 // Motor 2
+#define in_2_ph_1 24 // Motor 2
+#define in_3_ph_1 23 // Motor 1
+#define in_4_ph_1 22 // Motor 1
 
-// Define Ponte H 1:
-// Motor_1 == Front Left
-// Motor_2 == Back Left
-#define in_1_ph_1 25 // Motor_2
-#define in_2_ph_1 24 // Motor_2
-#define in_3_ph_1 23 // Motor_1
-#define in_4_ph_1 22 // Motor_1
+// Ponte H 2
+#define in_1_ph_2 47 // Motor 4
+#define in_2_ph_2 46 // Motor 4
+#define in_3_ph_2 49 // Motor 3
+#define in_4_ph_2 48 // Motor 3
 
-// Define Ponte H 2:
-// Motor_3 == Back Right
-// Motor_4 == Front Right
-#define in_1_ph_2 47 // Motor_4
-#define in_2_ph_2 46 // Motor_4
-#define in_3_ph_2 49 // Motor_3
-#define in_4_ph_2 48 // Motor_3
-
-// PWM:
-#define pwm_1 2 // Motor_1
-#define pwm_2 3 // Motor_2
-#define pwm_3 4 // Motor_3
-#define pwm_4 5 // Motor_4
+// PWM calibrado
+#define pwm_2 2 // Motor 2
+#define pwm_1 3 // Motor 1
+#define pwm_4 4 // Motor 4
+#define pwm_3 5 // Motor 3
 
 // Fim de curso
 #define fim_curso_pin 7
 
-// Se o fim de curso estiver ligado entre o pino e GND, deixe true.
-// Se estiver ligado entre o pino e 5V, troque para false e mude o pinMode se necessario.
+// Dribbler / ESC
+#define DRIBBLER_ESC_PIN 9
+#define DRIBBLER_MIN_US 1000
+#define DRIBBLER_MAX_US 2000
+
+Servo esc;
+int dribblerMicroAtual = DRIBBLER_MIN_US;
+bool dribblerLigado = false;
+
+// Fim de curso
 const bool FIM_CURSO_ATIVO_LOW = true;
 const unsigned long DEBOUNCE_FIM_CURSO_MS = 35;
 
-// Sensor ultrassonico
+// Ultrassonico
 #define trig_ultrassonico 30
 #define echo_ultrassonico 31
 
@@ -46,90 +48,46 @@ const unsigned long DEBOUNCE_FIM_CURSO_MS = 35;
 #define SIG A0
 #define EN 26
 
+#define led_pin 6
+
 int ldr[16];
 bool linhaBrancaDetectada = false;
 int sensorBranco = -1;
 
-// Antes estava 600. Baixei para detectar branco com leitura menor.
 const int LIMIAR_BRANCO = 500;
-
-// true: branco tem leitura maior que preto
-// false: branco tem leitura menor que preto
 const bool BRANCO_VALOR_MAIOR = true;
 
-#define led_pin 6
-
 const int DISTANCIA_PARAR_CM = 25;
-
-// Protecao contra parede muito perto
 const int DISTANCIA_PAREDE_PERTO_CM = 12;
 const unsigned long TEMPO_RE_PAREDE_MS = 350;
 
 bool habilitar_debug = false;
-
-// Variavel para saber se o robo ja pegou a bola
 bool bolaCapturada = false;
 
-// Direction of the ball
 int ballDirecao = 0;
-
-// Intensity of the ball
 int ballIntens = 0;
 
-// Filtro para confirmar a direcao da bola
-int ultimaDirecaoLida = -1;
-int contadorDirecao = 0;
-int direcaoConfirmada = 0;
+// Velocidades calibradas
+const int M1_FORWARD_SPEED = 220;
+const int M2_FORWARD_SPEED = 255;
+const int M3_FORWARD_SPEED = 140;
+const int M4_FORWARD_SPEED = 255;
 
-const int LEITURAS_PARA_CONFIRMAR = 3;
+const int M1_BACK_SPEED = 220;
+const int M2_BACK_SPEED = 255;
+const int M3_BACK_SPEED = 140;
+const int M4_BACK_SPEED = 255;
 
-// Center of the robot
-const int BALL_CENTER = 5;
+const int M1_LEFT_ROT_SPEED = 220;
+const int M2_LEFT_ROT_SPEED = 255;
+const int M3_LEFT_ROT_SPEED = 140;
+const int M4_LEFT_ROT_SPEED = 255;
 
-// Velocidades por motor para cada movimento
+const int M1_RIGHT_ROT_SPEED = 220;
+const int M2_RIGHT_ROT_SPEED = 255;
+const int M3_RIGHT_ROT_SPEED = 140;
+const int M4_RIGHT_ROT_SPEED = 255;
 
-// Frente
-const int M1_FORWARD_SPEED = 100;
-const int M2_FORWARD_SPEED = 100;
-const int M3_FORWARD_SPEED = 100;
-const int M4_FORWARD_SPEED = 100;
-
-// Re
-const int M1_BACK_SPEED = 100;
-const int M2_BACK_SPEED = 100;
-const int M3_BACK_SPEED = 100;
-const int M4_BACK_SPEED = 100;
-
-// Giro para direita
-const int M1_RIGHT_ROT_SPEED = 70;
-const int M2_RIGHT_ROT_SPEED = 70;
-const int M3_RIGHT_ROT_SPEED = 70;
-const int M4_RIGHT_ROT_SPEED = 70;
-
-// Giro para esquerda
-const int M1_LEFT_ROT_SPEED = 70;
-const int M2_LEFT_ROT_SPEED = 70;
-const int M3_LEFT_ROT_SPEED = 70;
-const int M4_LEFT_ROT_SPEED = 70;
-
-// Movimento lateral direita
-const int M1_RIGHT_SPEED = 120;
-const int M2_RIGHT_SPEED = 120;
-const int M3_RIGHT_SPEED = 120;
-const int M4_RIGHT_SPEED = 120;
-
-// Movimento lateral esquerda
-const int M1_LEFT_SPEED = 120;
-const int M2_LEFT_SPEED = 120;
-const int M3_LEFT_SPEED = 120;
-const int M4_LEFT_SPEED = 120;
-
-const int M1_SEARCH_SPEED = 65;
-const int M2_SEARCH_SPEED = 65;
-const int M3_SEARCH_SPEED = 65;
-const int M4_SEARCH_SPEED = 65;
-
-// Motor direction
 const int HORARIO = 1;
 const int ANTI_HORARIO = -1;
 const int PARADO = 0;
@@ -142,65 +100,15 @@ float anguloRobo = 0;
 unsigned long ultimoTempoGyro = 0;
 
 const int TOLERANCIA_NORTE = 5;
-const int NORTH_ROT_SPEED = 150;
-
-// Se o robo girar para o lado errado procurando o norte, troque para true
 bool GYRO_INVERTIDO = false;
 
-// Interface serial
+// Serial
 int telaSerial = 0;
-bool menuImpresso = false;
 unsigned long ultimoPrintSerial = 0;
 const unsigned long INTERVALO_SERIAL = 1000;
 
 String estadoRobo = "Iniciando";
 float ultimaDistanciaCm = 999;
-
-void mpu6050_iniciar();
-int16_t ler_gyro_z_raw();
-void calibrar_gyro();
-void atualizar_gyro();
-float normalizar_angulo(float angulo);
-float erro_para_norte();
-
-void ir_reader();
-bool fim_curso_acionado();
-bool leitura_fim_curso_bruta_acionada();
-void seguir_bola_simples();
-
-void motor_1(int sentido, int speed);
-void motor_2(int sentido, int speed);
-void motor_3(int sentido, int speed);
-void motor_4(int sentido, int speed);
-
-void move_foward();
-void move_back();
-void right_rotation();
-void left_rotation();
-void move_right();
-void move_left();
-void search_ball();
-void stop_robot();
-
-float ler_distancia_cm();
-bool parede_muito_perto();
-void afastar_da_parede();
-void olhar_para_norte();
-void andar_ate_distancia();
-
-void imprimir_menu_serial();
-void atualizar_serial();
-void imprimir_situacao_robo();
-void imprimir_sensor_ir();
-void imprimir_ultrassonico();
-void imprimir_mpu6050();
-void imprimir_ldr();
-
-void setMuxChannel(int canal);
-void lerLDR();
-bool detectarBranco();
-
-void debug_msg(String msg);
 
 void setup() {
   Serial.begin(115200);
@@ -220,6 +128,8 @@ void setup() {
   pinMode(pwm_3, OUTPUT);
   pinMode(pwm_4, OUTPUT);
 
+  dribbler_iniciar();
+
   pinMode(fim_curso_pin, INPUT_PULLUP);
 
   pinMode(trig_ultrassonico, OUTPUT);
@@ -231,7 +141,6 @@ void setup() {
   pinMode(S3, OUTPUT);
   pinMode(SIG, INPUT);
   pinMode(EN, OUTPUT);
-
   digitalWrite(EN, LOW);
 
   pinMode(led_pin, OUTPUT);
@@ -246,10 +155,12 @@ void setup() {
   anguloRobo = 0;
   ultimoTempoGyro = millis();
 
-  debug_msg("Robo Soccer Iniciado!");
+  stop_robot();
 
   estadoRobo = "Procurando bola";
   imprimir_menu_serial();
+
+  Serial.println("Robo Soccer iniciado com motores calibrados");
 }
 
 void loop() {
@@ -258,12 +169,12 @@ void loop() {
   atualizar_gyro();
   ir_reader();
 
-  // ===== LDR =====
   lerLDR();
   linhaBrancaDetectada = detectarBranco();
 
   if (linhaBrancaDetectada) {
     stop_robot();
+    dribbler_parar();
     estadoRobo = "Linha branca detectada!";
     digitalWrite(led_pin, HIGH);
     return;
@@ -271,18 +182,18 @@ void loop() {
     digitalWrite(led_pin, LOW);
   }
 
-  // ===== PAREDE MUITO PERTO =====
   if (!bolaCapturada && parede_muito_perto()) {
+    dribbler_parar();
     afastar_da_parede();
     return;
   }
 
-  // ===== FIM DE CURSO =====
   if (fim_curso_acionado() && !bolaCapturada) {
     bolaCapturada = true;
 
     estadoRobo = "Com a bola";
     stop_robot();
+    dribbler_segurar();
     delay(100);
 
     estadoRobo = "Voltando para o norte";
@@ -299,14 +210,14 @@ void loop() {
     return;
   }
 
-  // ===== JA PEGOU A BOLA =====
   if (bolaCapturada) {
     estadoRobo = "Parado";
     stop_robot();
+    dribbler_parar();
     return;
   }
 
-  // ===== MOVIMENTO NORMAL =====
+  dribbler_segurar();
   seguir_bola_simples();
 
   delay(60);
@@ -321,13 +232,37 @@ void ir_reader() {
   if (habilitar_debug) {
     Serial.print("Direcao: ");
     Serial.print(ballDirecao);
-    Serial.print("\tIntensidade: ");
-    Serial.print(ballIntens);
-    Serial.print("\tFim de curso raw: ");
-    Serial.print(digitalRead(fim_curso_pin));
-    Serial.print("\tFim de curso filtrado: ");
-    Serial.println(fim_curso_acionado() ? "ACIONADO" : "SOLTO");
+    Serial.print(" | Intensidade: ");
+    Serial.println(ballIntens);
   }
+}
+
+void seguir_bola_simples() {
+  if (ballDirecao == 0) {
+    estadoRobo = "Procurando bola";
+    right_rotation();
+    return;
+  }
+
+  if (ballDirecao == 5) {
+    estadoRobo = "Indo ate a bola";
+    move_foward();
+    return;
+  }
+
+  if (ballDirecao == 1 || ballDirecao == 2 || ballDirecao == 3 || ballDirecao == 4) {
+    estadoRobo = "Bola a esquerda";
+    left_rotation();
+    return;
+  }
+
+  if (ballDirecao == 6 || ballDirecao == 7 || ballDirecao == 8 || ballDirecao == 9) {
+    estadoRobo = "Bola a direita";
+    right_rotation();
+    return;
+  }
+
+  stop_robot();
 }
 
 bool leitura_fim_curso_bruta_acionada() {
@@ -359,64 +294,70 @@ bool fim_curso_acionado() {
   return estadoFiltrado;
 }
 
-void seguir_bola_simples() {
-  if (ballDirecao == 0) {
-    estadoRobo = "Procurando bola";
-    right_rotation();
-    return;
-  }
+// ===== DRIBBLER / ESC NOVO =====
 
-  if (ballDirecao > 3 && ballDirecao < 7) {
-    estadoRobo = "Indo ate a bola";
-    move_foward();
-    return;
-  }
-
-  if (ballDirecao < 4) {
-    estadoRobo = "Bola a esquerda";
-    left_rotation();
-    return;
-  }
-
-  if (ballDirecao > 6) {
-    estadoRobo = "Bola a direita";
-    right_rotation();
-    return;
-  }
-
-  stop_robot();
+void dribbler_iniciar() {
+  esc.attach(DRIBBLER_ESC_PIN);
+  esc.writeMicroseconds(DRIBBLER_MIN_US);
+  dribblerMicroAtual = DRIBBLER_MIN_US;
+  dribblerLigado = false;
+  delay(3000);
 }
 
-void debug_msg(String msg) {
-  if (habilitar_debug) {
-    Serial.println(msg);
-  }
+void dribbler_segurar() {
+  esc.writeMicroseconds(DRIBBLER_MAX_US);
+  dribblerMicroAtual = DRIBBLER_MAX_US;
+  dribblerLigado = true;
 }
 
-// Motor_1 == Front Left
+void dribbler_chutar() {
+  for (int velocidade = DRIBBLER_MIN_US; velocidade <= DRIBBLER_MAX_US; velocidade += 10) {
+    esc.writeMicroseconds(velocidade);
+    dribblerMicroAtual = velocidade;
+    delay(50);
+  }
+
+  delay(3000);
+
+  for (int velocidade = DRIBBLER_MAX_US; velocidade >= DRIBBLER_MIN_US; velocidade -= 10) {
+    esc.writeMicroseconds(velocidade);
+    dribblerMicroAtual = velocidade;
+    delay(50);
+  }
+
+  dribblerLigado = false;
+}
+
+void dribbler_parar() {
+  esc.writeMicroseconds(DRIBBLER_MIN_US);
+  dribblerMicroAtual = DRIBBLER_MIN_US;
+  dribblerLigado = false;
+}
+
+// ===== MOTORES =====
+
 void motor_1(int sentido, int speed) {
   if (sentido == HORARIO) {
-    digitalWrite(in_4_ph_1, LOW);
     digitalWrite(in_3_ph_1, HIGH);
-  } else if (sentido == ANTI_HORARIO) {
-    digitalWrite(in_4_ph_1, HIGH);
-    digitalWrite(in_3_ph_1, LOW);
-  } else {
     digitalWrite(in_4_ph_1, LOW);
+  } else if (sentido == ANTI_HORARIO) {
     digitalWrite(in_3_ph_1, LOW);
+    digitalWrite(in_4_ph_1, HIGH);
+  } else {
+    digitalWrite(in_3_ph_1, LOW);
+    digitalWrite(in_4_ph_1, LOW);
   }
 
   analogWrite(pwm_1, speed);
 }
 
-// Motor_2 == Back Left
 void motor_2(int sentido, int speed) {
   if (sentido == HORARIO) {
-    digitalWrite(in_1_ph_1, LOW);
-    digitalWrite(in_2_ph_1, HIGH);
-  } else if (sentido == ANTI_HORARIO) {
     digitalWrite(in_1_ph_1, HIGH);
     digitalWrite(in_2_ph_1, LOW);
+  } else if (sentido == ANTI_HORARIO) {
+    digitalWrite(in_1_ph_1, LOW);
+    digitalWrite(in_2_ph_1, HIGH);
   } else {
     digitalWrite(in_1_ph_1, LOW);
     digitalWrite(in_2_ph_1, LOW);
@@ -425,7 +366,6 @@ void motor_2(int sentido, int speed) {
   analogWrite(pwm_2, speed);
 }
 
-// Motor_3 == Back Right
 void motor_3(int sentido, int speed) {
   if (sentido == HORARIO) {
     digitalWrite(in_3_ph_2, HIGH);
@@ -441,7 +381,6 @@ void motor_3(int sentido, int speed) {
   analogWrite(pwm_3, speed);
 }
 
-// Motor_4 == Front Right
 void motor_4(int sentido, int speed) {
   if (sentido == HORARIO) {
     digitalWrite(in_1_ph_2, LOW);
@@ -456,6 +395,43 @@ void motor_4(int sentido, int speed) {
 
   analogWrite(pwm_4, speed);
 }
+
+void move_foward() {
+  motor_1(ANTI_HORARIO, M1_FORWARD_SPEED);
+  motor_2(ANTI_HORARIO, M2_FORWARD_SPEED);
+  motor_3(HORARIO, M3_FORWARD_SPEED);
+  motor_4(ANTI_HORARIO, M4_FORWARD_SPEED);
+}
+
+void move_back() {
+  motor_1(HORARIO, M1_BACK_SPEED);
+  motor_2(HORARIO, M2_BACK_SPEED);
+  motor_3(ANTI_HORARIO, M3_BACK_SPEED);
+  motor_4(HORARIO, M4_BACK_SPEED);
+}
+
+void left_rotation() {
+  motor_1(ANTI_HORARIO, M1_LEFT_ROT_SPEED);
+  motor_2(HORARIO, M2_LEFT_ROT_SPEED);
+  motor_3(ANTI_HORARIO, M3_LEFT_ROT_SPEED);
+  motor_4(ANTI_HORARIO, M4_LEFT_ROT_SPEED);
+}
+
+void right_rotation() {
+  motor_1(HORARIO, M1_RIGHT_ROT_SPEED);
+  motor_2(ANTI_HORARIO, M2_RIGHT_ROT_SPEED);
+  motor_3(HORARIO, M3_RIGHT_ROT_SPEED);
+  motor_4(HORARIO, M4_RIGHT_ROT_SPEED);
+}
+
+void stop_robot() {
+  motor_1(PARADO, 0);
+  motor_2(PARADO, 0);
+  motor_3(PARADO, 0);
+  motor_4(PARADO, 0);
+}
+
+// ===== MPU6050 =====
 
 void mpu6050_iniciar() {
   Wire.beginTransmission(MPU_ADDR);
@@ -483,7 +459,7 @@ int16_t ler_gyro_z_raw() {
 }
 
 void calibrar_gyro() {
-  debug_msg("Calibrando MPU6050... deixe o robo parado");
+  Serial.println("Calibrando MPU6050... deixe o robo parado");
 
   long soma = 0;
   const int amostras = 1000;
@@ -495,7 +471,7 @@ void calibrar_gyro() {
 
   gyroZOffset = soma / (float)amostras;
 
-  debug_msg("Calibracao concluida");
+  Serial.println("Calibracao concluida");
 }
 
 void atualizar_gyro() {
@@ -510,7 +486,6 @@ void atualizar_gyro() {
   ultimoTempoGyro = agora;
 
   int16_t gyroZRaw = ler_gyro_z_raw();
-
   float velocidadeZ = (gyroZRaw - gyroZOffset) / 131.0;
 
   if (GYRO_INVERTIDO) {
@@ -552,7 +527,6 @@ void olhar_para_norte() {
 
     if (erroAbs <= TOLERANCIA_NORTE) {
       stop_robot();
-      debug_msg("Norte encontrado!");
       delay(300);
       return;
     }
@@ -565,7 +539,6 @@ void olhar_para_norte() {
 
     if (millis() - inicio > 6000) {
       stop_robot();
-      debug_msg("Tempo limite procurando norte");
       return;
     }
 
@@ -573,61 +546,7 @@ void olhar_para_norte() {
   }
 }
 
-void move_foward() {
-  motor_1(ANTI_HORARIO, M1_FORWARD_SPEED);
-  motor_2(ANTI_HORARIO, M2_FORWARD_SPEED);
-  motor_3(HORARIO, M3_FORWARD_SPEED);
-  motor_4(HORARIO, M4_FORWARD_SPEED);
-}
-
-void move_back() {
-  motor_1(HORARIO, M1_BACK_SPEED);
-  motor_2(HORARIO, M2_BACK_SPEED);
-  motor_3(ANTI_HORARIO, M3_BACK_SPEED);
-  motor_4(ANTI_HORARIO, M4_BACK_SPEED);
-}
-
-void right_rotation() {
-  motor_1(ANTI_HORARIO, M1_RIGHT_ROT_SPEED);
-  motor_2(ANTI_HORARIO, M2_RIGHT_ROT_SPEED);
-  motor_3(ANTI_HORARIO, M3_RIGHT_ROT_SPEED);
-  motor_4(ANTI_HORARIO, M4_RIGHT_ROT_SPEED);
-}
-
-void left_rotation() {
-  motor_1(HORARIO, M1_LEFT_ROT_SPEED);
-  motor_2(HORARIO, M2_LEFT_ROT_SPEED);
-  motor_3(HORARIO, M3_LEFT_ROT_SPEED);
-  motor_4(HORARIO, M4_LEFT_ROT_SPEED);
-}
-
-void move_right() {
-  motor_1(ANTI_HORARIO, M1_RIGHT_SPEED);
-  motor_2(HORARIO, M2_RIGHT_SPEED);
-  motor_3(HORARIO, M3_RIGHT_SPEED);
-  motor_4(ANTI_HORARIO, M4_RIGHT_SPEED);
-}
-
-void move_left() {
-  motor_1(HORARIO, M1_LEFT_SPEED);
-  motor_2(ANTI_HORARIO, M2_LEFT_SPEED);
-  motor_3(ANTI_HORARIO, M3_LEFT_SPEED);
-  motor_4(HORARIO, M4_LEFT_SPEED);
-}
-
-void search_ball() {
-  motor_1(ANTI_HORARIO, M1_SEARCH_SPEED);
-  motor_2(ANTI_HORARIO, M2_SEARCH_SPEED);
-  motor_3(ANTI_HORARIO, M3_SEARCH_SPEED);
-  motor_4(ANTI_HORARIO, M4_SEARCH_SPEED);
-}
-
-void stop_robot() {
-  motor_1(PARADO, 0);
-  motor_2(PARADO, 0);
-  motor_3(PARADO, 0);
-  motor_4(PARADO, 0);
-}
+// ===== ULTRASSONICO =====
 
 float ler_distancia_cm() {
   long duracao;
@@ -647,7 +566,6 @@ float ler_distancia_cm() {
   }
 
   distancia = duracao * 0.034 / 2;
-
   return distancia;
 }
 
@@ -678,6 +596,7 @@ void afastar_da_parede() {
 void andar_ate_distancia() {
   estadoRobo = "Indo para o gol";
   digitalWrite(led_pin, LOW);
+  dribbler_segurar();
 
   while (true) {
     atualizar_serial();
@@ -688,17 +607,63 @@ void andar_ate_distancia() {
 
     if (distancia > 0 && distancia <= DISTANCIA_PARAR_CM) {
       stop_robot();
-      estadoRobo = "Parado no gol";
+      estadoRobo = "Chutando";
       digitalWrite(led_pin, HIGH);
+      dribbler_chutar();
+      estadoRobo = "Parado no gol";
       delay(300);
       return;
     }
 
+    dribbler_segurar();
     move_foward();
 
     delay(60);
   }
 }
+
+// ===== LDR =====
+
+void setMuxChannel(int canal) {
+  digitalWrite(S0, canal & 0x01);
+  digitalWrite(S1, (canal >> 1) & 0x01);
+  digitalWrite(S2, (canal >> 2) & 0x01);
+  digitalWrite(S3, (canal >> 3) & 0x01);
+}
+
+void lerLDR() {
+  for (int i = 0; i < 16; i++) {
+    setMuxChannel(i);
+
+    delayMicroseconds(300);
+
+    analogRead(SIG);
+    ldr[i] = analogRead(SIG);
+  }
+}
+
+bool detectarBranco() {
+  sensorBranco = -1;
+
+  for (int i = 0; i < 16; i++) {
+    bool brancoDetectado;
+
+    if (BRANCO_VALOR_MAIOR) {
+      brancoDetectado = ldr[i] > LIMIAR_BRANCO;
+    } else {
+      brancoDetectado = ldr[i] < LIMIAR_BRANCO;
+    }
+
+    if (brancoDetectado) {
+      sensorBranco = i;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// ===== SERIAL =====
 
 void imprimir_menu_serial() {
   Serial.println();
@@ -779,6 +744,11 @@ void imprimir_situacao_robo() {
   Serial.print("| Distancia: ");
   Serial.print(ultimaDistanciaCm);
   Serial.println(" cm");
+  Serial.print("| Dribbler: ");
+  Serial.print(dribblerLigado ? "LIGADO" : "PARADO");
+  Serial.print(" / ");
+  Serial.print(dribblerMicroAtual);
+  Serial.println(" us");
   Serial.println("+-------------------------------+");
 }
 
@@ -857,43 +827,4 @@ void imprimir_ldr() {
   Serial.println(BRANCO_VALOR_MAIOR ? "SIM" : "NAO");
 
   Serial.println("+-------------------------------+");
-}
-
-void setMuxChannel(int canal) {
-  digitalWrite(S0, canal & 0x01);
-  digitalWrite(S1, (canal >> 1) & 0x01);
-  digitalWrite(S2, (canal >> 2) & 0x01);
-  digitalWrite(S3, (canal >> 3) & 0x01);
-}
-
-void lerLDR() {
-  for (int i = 0; i < 16; i++) {
-    setMuxChannel(i);
-
-    delayMicroseconds(300);
-
-    analogRead(SIG);
-    ldr[i] = analogRead(SIG);
-  }
-}
-
-bool detectarBranco() {
-  sensorBranco = -1;
-
-  for (int i = 0; i < 16; i++) {
-    bool brancoDetectado;
-
-    if (BRANCO_VALOR_MAIOR) {
-      brancoDetectado = ldr[i] > LIMIAR_BRANCO;
-    } else {
-      brancoDetectado = ldr[i] < LIMIAR_BRANCO;
-    }
-
-    if (brancoDetectado) {
-      sensorBranco = i;
-      return true;
-    }
-  }
-
-  return false;
 }
